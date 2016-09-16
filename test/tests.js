9aae8bba43773cfb7aff527c8757770ca6a55d77
Jason Holtkamp <holtkam2@gmail.com>
2016-09-15 21:48:02 -0500

    configured for testing

---
+++ test/tests.js
@@ -0,0 +1,19 @@
+var chai = require('chai');
+
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+describe('the crowdCart2 user interface', function() {
+
+  //put all your tests related to the front end in here! (stuff in the client folder)
+
+});
+
+
+
+describe('the crowdCart2 server', function() {
+
+  //put all your tests related to the back end in here!(stuff in the server folder)
+
+});
